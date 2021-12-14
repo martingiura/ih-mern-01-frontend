7d3d2af0135bc08f858c5fdc0544fd6874b6a2bc
Martin Giura <martingiura.dev@gmail.com>
2021-12-14 13:22:05 -0600

    inicio de ruteo

---
+++ src/components/Guitars/index.js
@@ -0,0 +1,6 @@
+// ./client/src/Guitars/index.js
+import React from "react";
+
+export default function Guitars() {
+  return <div>Listado de guitarras</div>;
+}
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,6 @@
+// ./client/src/Layout/Header.js
+import React from "react";
+
+export default function Header() {
+  return <div>Este es el header</div>;
+}
